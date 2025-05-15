0a71d5e593d27b93648fec23e1565f3f34463af2
Diwakar Singh <diwakar.sharajman.com>
2025-05-15 15:13:57 +0530

    Configured .env

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from config import settings
 
+#URL_DATABASE = 'postgresql://quiz_dev:Diwakar123456@localhost:5433/quiz_dev'
 
-URL_DATABASE = 'postgresql://quiz_dev:Diwakar123456@localhost:5433/quiz_dev'
+URL_DATABASE = (f"postgresql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}")
 
 engine = create_engine(URL_DATABASE)
 
